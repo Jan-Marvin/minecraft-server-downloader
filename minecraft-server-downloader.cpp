d79194b96ecc0ccbb8b13196f53badd9e102fa3a
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2021-04-26 20:34:08 +0200

    Fix inaccurate download speed on linux

--- minecraft-server-downloader.cpp
+++ minecraft-server-downloader.cpp
@@ -12,7 +12,7 @@
 
 #include "minecraft-server-downloader.h"
 
-clock_t start_timer;
+std::chrono::system_clock::time_point start_timer;
 
 //write data to file
 size_t write_data_file_callback(void* ptr, size_t size, size_t nmemb, FILE* stream) {
@@ -51,6 +51,9 @@ int getspace(int dltotal, int dlnow) {
 
 //progress bar
 int progress_callback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow) {
+	if (start_timer.time_since_epoch().count() == 0) {
+		start_timer = std::chrono::system_clock::now();
+	}
 	std::string bar;
 	int nrspace = getspace(dltotal, dlnow);
 	for (int i = 0; i < (10.0 / dltotal) * dlnow; i++) {
@@ -66,9 +69,8 @@ int progress_callback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_
 			space.append(" ");
 		}
 	}
-	clock_t clockTicksTaken = clock() - start_timer;
-	float timeInSeconds = clockTicksTaken / (float)CLOCKS_PER_SEC;
-	float speed = (dlnow / 1e+6) / timeInSeconds;
+	std::chrono::duration<float, std::milli> diff = std::chrono::system_clock::now() - start_timer;
+	float speed = (dlnow / 1e+6) / (diff.count() / 1000);
 	printf("\r[%s]%s%.2f/%.2f MB   %.2f MB/s     ", bar.c_str(), space.c_str(), ((float)dlnow / 1e+6), ((float)dltotal / 1e+6), speed);
 	return CURLE_OK;
 }
